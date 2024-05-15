b3c00b8b8d9321b0ce75e3b0e4e1944d1ede42d8
MoeMahhouk <mohammed-mahhouk@hotmail.com>
2024-05-15 11:10:40 +0200

    Add new script to compare a given builder pubkey to the winner for each slot

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # rbuilder-relay-measurement
 A script to pull data for a specific block number and builder public key from the relay and compare them against those from the builder logs
 
-# Example
-```python fetchData.py 0xa1aa1ad4f2ad89f3cd2da667259022763ce7d22f75582fc824cd6cc8c56a89edb6a922e64ab95666f4bdeb2c309052ef 19867351 builder.json```
+# Example for compare_builder_relay.py
+```python compare_builder_relay.py 0xa1aa1ad4f2ad89f3cd2da667259022763ce7d22f75582fc824cd6cc8c56a89edb6a922e64ab95666f4bdeb2c309052ef 19867351 builder.json```
 
 # Preparations
 1. fetch the builder logs for the specific relay and preferably filter for a specific block number (see builder.json), for example
@@ -11,3 +11,5 @@ journalctl --since "1 hour ago" -u rbuilder -o cat | grep  "\"relay\":\"ultrasou
 ```
 2. run the script with the builders pubkey, block number and the builder.json that you extracted from the previous step. 
 
+# Example for compare_winenr.py
+```python compare_winner.py 0xa1aa1ad4f2ad89f3cd2da667259022763ce7d22f75582fc824cd6cc8c56a89edb6a922e64ab95666f4bdeb2c309052ef```
---
+++ compare_winner.py
@@ -0,0 +1,62 @@
+import requests
+import argparse
+
+def fetch_data(builder_pubkey, limit):
+    url = "https://relay-analytics.ultrasound.money/relay/v1/data/bidtraces/builder_blocks_received"
+    params = {
+        'builder_pubkey': builder_pubkey,
+        'limit': limit
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    slots_data = {}
+    for item in data:
+        if item['slot'] in slots_data:
+            slots_data[item['slot']].append(item)
+        else:
+            slots_data[item['slot']] = [item]
+    return slots_data
+
+def fetch_and_compare(slot, builder_data_list):
+    url = "https://relay-analytics.ultrasound.money/relay/v1/data/bidtraces/proposer_payload_delivered"
+    params = {
+        'slot': slot
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    if data:
+        url = "https://relay-analytics.ultrasound.money/relay/v1/data/bidtraces/builder_blocks_received"
+        params = {
+            'builder_pubkey': data[0]['builder_pubkey'],
+            'slot': slot,
+            'block_hash': data[0]['block_hash']
+        }
+        response = requests.get(url, params=params)
+        winner_data = response.json()
+
+        print(f"Comparing for slot {slot}")
+        for i, builder_data in enumerate(builder_data_list):
+            print(f"Comparing builder's {i+1} submission against the slot's winner")
+            if builder_data['builder_pubkey'] == winner_data[0]['builder_pubkey']:
+                print("Your builder pubkey won this slot!")
+                break
+            print(f"Value difference: {int(winner_data[0]['value']) - int(builder_data['value'])}")
+            print(f"Gas used difference: {int(winner_data[0]['gas_used']) - int(builder_data['gas_used'])}")
+            print(f"Num_tx difference: {int(winner_data[0]['num_tx']) - int(builder_data['num_tx'])}")
+            print(f"Timestamp difference: {int(winner_data[0]['timestamp_ms']) - int(builder_data['timestamp_ms'])}")
+            print()  # Add an empty line for better readability
+
+def main():
+    parser = argparse.ArgumentParser(description='Fetch and compare data.')
+    parser.add_argument('builder_pubkey', type=str, help='Builder public key')
+    parser.add_argument('--limit', type=int, default=500, help='Limit for the number of records to fetch')
+    args = parser.parse_args()
+
+    slots_data = fetch_data(args.builder_pubkey, args.limit)
+    for slot, builder_data_list in slots_data.items():
+        fetch_and_compare(slot, builder_data_list)
+
+if __name__ == "__main__":
+    main()
+
+
